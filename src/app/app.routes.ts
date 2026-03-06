2d21e9903d4d9f988db079b9ae5d86b526f325b6
GirishKanKi <girishkanki1203@gmai.com>
2026-03-06 13:08:32 +0530

    pushing code to github

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'user-login',
+        loadChildren: () => import('./modules/user-login/user-login.module').then(m => m.UserLoginModule)
+    },
+    {
+        path:'',
+        loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterModule)
+    }
+];
