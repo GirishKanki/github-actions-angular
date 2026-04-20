a1fa13e34aa12b78658ed218650f8cd9960ac52c
GirishKanKi <girishkanki1203@gmai.com>
2026-04-20 14:51:58 +0530

    new environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    recaptcha: {
+      siteKey: '6Ldt_pwqAAAAABqo3PsiGWGkYaEdexQCjfBztjvv'
+  },
+    serverUrl: 'http://localhost:8080/v1/',
+    imageUrl: 'http://localhost:8080/v1/file/image/',
+    pdfUrl: 'http://localhost:8080/v1/file/document/',
+    websiteUrl: 'http://localhost:4200/'
+  };
